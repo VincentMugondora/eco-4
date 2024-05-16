6a8d674f190fd91ad0553d747d584cdb5d1705a2
VincentMugondora <vinmugondora@gmail.com>
2024-05-16 11:26:42 +0200

    updated

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 const menu = document.querySelector(".hamburger-menu")
 const sliding = document.querySelector(".sliding")
+const linkTags = document.querySelectorAll(".link-tag")
 
 menu.addEventListener("click", function(){
     sliding.classList.toggle("sliders")
+})
+
+linkTags.forEach((tag)=>{
+    // console.log("here",tag)
+
+
+    
+    tag.addEventListener('click', ()=>{
+        sliding.classList.toggle("sliders")
+    })
 })
